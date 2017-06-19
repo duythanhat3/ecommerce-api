d2a85bdfe6b0e0107ff9dd2feea349704605176c
VuDuyThanh <thanhjv87@gmail.com>
2017-06-19 14:54:34 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Install**
+
+`npm install`
+
+`edit /server/datasources.json`
+
+`create sample models from /server/boot/create-sample-models.js`
---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,135 @@
+module.exports = function(app) {
+
+    /*
+    app.dataSources.mysqlDs.automigrate('Banner', function(err) {
+        if (err) throw err;
+
+        app.models.Banner.create([{
+            name: 'Father Day Sale',
+            title: 'Father Day Sale | 15% Off Select Men Gear + Free Shipping Over $99',
+            active: true
+        }, ], function(err, banner) {
+            if (err) throw err;
+
+            console.log('Models created: \n', banner);
+        });
+    });
+
+
+    app.dataSources.mysqlDs.automigrate('Account', function(err) {
+        if (err) throw err;
+
+        app.models.Account.create([{
+            username: 'admin',
+            email: 'admin@gmail.com',
+            password: 'admin'
+        }, ], function(err, banner) {
+            if (err) throw err;
+
+            console.log('Models created: \n', banner);
+        });
+    });
+
+    app.dataSources.mysqlDs.automigrate('Menu', function(err) {
+        if (err) throw err;
+
+        app.models.Menu.create([{
+            name: 'News',
+            link: 'http://seudo.vn',
+            is_root: true,
+            active: true
+        }, ], function(err, model) {
+            if (err) throw err;
+
+            console.log('Models created: \n', model);
+        });
+    });
+
+    app.dataSources.mysqlDs.automigrate('Slider', function (err) {
+        if (err) throw err;
+
+        app.models.Slider.create([
+            {
+                name: 'Carolina Panthers',
+                link: 'http://seudo.vn',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                price: 30000
+            },
+            {
+                name: 'Carolina Panthers',
+                link: 'http://seudo.vn',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                price: 30000
+            },
+            {
+                name: 'Carolina Panthers',
+                link: 'http://seudo.vn',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                price: 30000
+            },
+            {
+                name: 'Carolina Panthers',
+                link: 'http://seudo.vn',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                price: 30000
+            },
+            {
+                name: 'Carolina Panthers',
+                link: 'http://seudo.vn',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                price: 30000
+            }
+            ,], function (err, model) {
+            if (err) throw err;
+
+            console.log('Models created: \n', model);
+        });
+    });
+
+    app.dataSources.mysqlDs.automigrate('Product', function (err) {
+        if (err) throw err;
+
+        app.models.Product.create([
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            },
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            },
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            },
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            },
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            },
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            },
+            {
+                name: 'Product',
+                image: 'https://cdn3.bigcommerce.com/s-47wj5jh8xv/products/26597/images/41728/180-1729__87148.1497471980.200.250.png',
+                active: true
+            }
+            ,], function (err, model) {
+            if (err) throw err;
+
+            console.log('Models created: \n', model);
+        });
+    });
+    */
+};
